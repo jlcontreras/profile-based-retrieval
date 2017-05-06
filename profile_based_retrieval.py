f23d13d4934099784accfe5e25c7e773eb5a63bd
jose <joseluis.contreras.santos@gmail.com>
2017-05-06 14:25:28 +0200

    Fixed bugs: it was not working properly

--- profile_based_retrieval.py
+++ profile_based_retrieval.py
@@ -1,6 +1,7 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.datasets import fetch_20newsgroups
+import heapq, operator
 
 # Using Stemmer from the PyStemmer package because it's faster than the nltk stemmer
 # Package can be downloaded from here: https://pypi.python.org/pypi/PyStemmer
@@ -19,7 +20,7 @@ documents = fetch_20newsgroups()
 # Sample queries representing the interests of our users 
 queries = {"soccer": "soccer goal league championship striker player score coach football",
 		   "music": "music album cd lp song singer play listen genre album band",
-		   "cars": "car motor fuel petrol cylinder steering drive hybrid chassis",
+		   "cars": "car motor fuel petrol cylinder steering drive hybrid chassis engine mph",
 		   "films": "film movie actor director role genre scene camera",
 		   "polytics": "polytics government candidate campaign president election minister votation"}
 
@@ -27,23 +28,25 @@ queries = {"soccer": "soccer goal league championship striker player score coach
 
 # TODO: We have to decide wether to smooth idf or not, and why
 tfidf_vectorizer = StemmedTfidfVectorizer(smooth_idf = True)
-tfidf_matrix = tfidf_vectorizer.fit_transform(documents)
+tfidf_matrix = tfidf_vectorizer.fit_transform(documents.data)
 
 # Modify the params of the vectorizer so that it accepts content instead of paths
 # params = tfidf_vectorizer.get_params()
 # params["input"] = "content"
 # tfidf_vectorizer.set_params(**params)
-q_matrix = tfidf_vectorizer.transform(queries.values)
+q_matrix = tfidf_vectorizer.transform(queries.values())
 
 # TODO: Select query via command line argument
-distances = cosine_similarity(q_matrix[0:1], tfidf_matrix)
+distances = cosine_similarity(q_matrix[1:2], tfidf_matrix)[0]
 
 # Top N most similar results (removing the first one because it's the same one, cos = 1)
 n = 3
-top_indexes = [list(distances[0]).index(x) for x in sorted(list(distances[0]), reverse=True)[1:n+1]]
+#top_indexes = [list(distances[0]).index(x) for x in sorted(list(distances[0]), reverse=True)[1:n+1]]
+top_indexes = zip(*heapq.nlargest(3, enumerate(distances), key=operator.itemgetter(1)))[0]
 
 print("Top {0} matches: \n------------------------------".format(n))
 for i in top_indexes:
-	print(documents.data[n])
+	print("Index: {0} ----------------------------".format(i))
+	print(documents.data[i])
 
 
